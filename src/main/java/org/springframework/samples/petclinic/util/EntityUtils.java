aed724ace7c6818b504053b28512c1eabea86096
igorcoval <igorcoval@gmail.com>
2016-03-25 10:42:22 +0300

    test commit 2

--- src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
@@ -45,7 +45,7 @@ public abstract class EntityUtils {
         throws ObjectRetrievalFailureException {
         for (T entity : entities) {
             if (entity.getId() == entityId && entityClass.isInstance(entity)) {
-                System.out.println("");
+                System.out.println("_");
                 return entity;
             }
         }
